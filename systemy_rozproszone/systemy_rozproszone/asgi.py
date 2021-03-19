3f8c471d19ffb0b211b76d818dfa4826846f4ec4
Adrian Piwnicki <piwnicki.adrian@gmail.com>
2021-03-19 16:18:21 +0100

    proba

---
+++ systemy_rozproszone/systemy_rozproszone/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for systemy_rozproszone project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'systemy_rozproszone.settings')
+
+application = get_asgi_application()
